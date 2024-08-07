4c45cbf81e968018e1513a9ed4fe236b37104a35
Jay Tandia <jaytandia50@kgpian.iitkgp.ac.in>
2024-08-07 10:12:41 +0530

    change done

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,7 +3,7 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://neondb_owner:txdCuSkIO8Y6@ep-frosty-mode-a5e5t660.us-east-2.aws.neon.tech/ai-interview-mocker?sslmode=require',
+      url: 'NEXT_PUBLIC_DRIZZLE_DB_URL',
     }
   };
   
